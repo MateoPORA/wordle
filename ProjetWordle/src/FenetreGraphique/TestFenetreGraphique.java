e06b6fe4003a316a81a2c59e8722416adcb80950
chiaralns <105419631+chiaralns@users.noreply.github.com>
2022-05-12 11:52:56 +0200

    Update TestFenetreGraphique.java

--- ProjetWordle/src/FenetreGraphique/TestFenetreGraphique.java
+++ ProjetWordle/src/FenetreGraphique/TestFenetreGraphique.java
@@ -25,31 +25,30 @@ public class TestFenetreGraphique {
         
         Matus w=new Matus("petitLarousse.txt");
         //w.showMotS();
-        
-        FenetreGraphique fenetre = new FenetreGraphique("WORDLE", 500, 400);
+        while(w.getTentatives()>0 && !w.getVictoire()){
+            w.proposition();
+            FenetreGraphique fenetre = new FenetreGraphique("WORDLE", 500, 400);
         //fenetre.getGraphics2D().setColor(Color.RED);
         //fenetre.getGraphics2D().drawLine(10, 10, 60, 160);
 
-        fenetre.getGraphics2D().setColor(Color.RED);
-        fenetre.getGraphics2D().drawRect(18, 100, 460, 282);
+             fenetre.getGraphics2D().setColor(Color.RED);
+             fenetre.getGraphics2D().drawRect(18, 100, 460, 282);
 
-        fenetre.getGraphics2D().setColor(Color.BLUE);
+             fenetre.getGraphics2D().setColor(Color.BLUE);
         //fenetre.getGraphics2D().fillOval(200, 10, 20, 10);
 
-        fenetre.getGraphics2D().drawString("WORDLE", 220, 20);
-        fenetre.getGraphics2D().drawString("Vous cherchez:", 20, 50);
-        fenetre.getGraphics2D().drawString(w.getTiret(), 50, 50);
+             fenetre.getGraphics2D().drawString("WORDLE", 220, 20);
+             fenetre.getGraphics2D().drawString("Vous cherchez:", 20, 50);
+             fenetre.getGraphics2D().drawString(w.getTiret(), 120, 50);
+             fenetre.getGraphics2D().drawString("Tentatives restantes:", 300, 50);
+             fenetre.getGraphics2D().drawString(w.toString(), 440, 50);
 
-        fenetre.actualiser();
+             fenetre.actualiser();
 
-        fenetre.enregistrerImage("test.png");
-        
-        while(w.getTentatives()>0 && !w.getVictoire()){
-            w.proposition();
-            
+             fenetre.enregistrerImage("test.png");
+             
         }
         
      }
 
 }
-
